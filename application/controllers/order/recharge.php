f05f4d248294d748d6da2870e062b20f32a18faa
johnnyeven_work <johnnyeven@gmail.com>
2013-09-09 16:36:48 +0800

    1

---
+++ application/controllers/order/recharge.php
@@ -0,0 +1,52 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Recharge extends CI_Controller
+{
+	private $pageName = 'order/recharge';
+	private $user = null;
+
+	public function __construct()
+	{
+		parent::__construct ();
+		$this->load->model('utils/check_user', 'check');
+		$this->user = $this->check->validate();
+	}
+	
+	public function index()
+	{
+		$this->load->model('mserver');
+		
+		$serverResult = $this->mserver->read();
+		$data = array(
+			'admin'			=>	$this->user,
+			'page_name'	=>	$this->pageName,
+			'server'			=>	$serverResult
+		);
+		$this->render->render($this->pageName, $data);
+	}
+	
+	public function lists($provider = 'highchart')
+	{
+		$this->load->model('utils/return_format');
+		$accountdb = $this->load->database('accountdb', TRUE);
+		
+		$serverId = $this->input->post('server_id');
+		if(!empty($serverId))
+		{
+			$sql = "SELECT `account_job`, count(*) as `count` FROM `web_account` WHERE `server_id`='{$serverId}' GROUP BY `account_job`";
+		}
+		else
+		{
+			$sql = "SELECT `account_job`, count(*) as `count` FROM `web_account` GROUP BY `account_job`";
+		}
+		$result = $accountdb->query($sql)->result_array();
+		for($i=0; $i<count($result); $i++)
+		{
+			$result[$i] = array_values($result[$i]);
+		}
+		
+		echo $this->return_format->format($result);
+	}
+}
+
+?>
---
+++ application/models/mconsume.php
@@ -0,0 +1,128 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+require_once('ICrud.php');
+
+class Mconsume extends CI_Model implements ICrud
+{
+	
+	private $accountTable = 'log_consume';
+	private $logdb = null;
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->logdb = $this->load->database('logdb', TRUE);
+	}
+	
+	public function count($parameter = null, $extension = null)
+	{
+		if(!empty($parameter))
+		{
+			foreach($parameter as $key=>$value)
+			{
+				$this->logdb->where($key, $value);
+			}
+		}
+		if(!empty($extension))
+		{
+			
+		}
+		return $this->logdb->count_all_results($this->accountTable);
+	}
+	
+	public function create($row)
+	{
+		if(!empty($row))
+		{
+			if($this->logdb->insert($this->accountTable, $row))
+			{
+				return $this->logdb->insert_id();
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public function read($parameter = null, $extension = null, $limit = 0, $offset = 0)
+	{
+		if(!empty($parameter))
+		{
+			foreach($parameter as $key=>$value)
+			{
+				$this->logdb->where($key, $value);
+			}
+		}
+		if(!empty($extension))
+		{
+			if(!empty($extension['select']) && is_array($extension['select']))
+			{
+				$this->logdb->select($extension['select']);
+			}
+			if(!empty($extension['group_by']))
+			{
+				$this->logdb->group_by($extension['group_by'][0], $extension['group_by'][1]);
+			}
+			if(!empty($extension['order_by']))
+			{
+				$this->logdb->order_by($extension['order_by'][0], $extension['order_by'][1]);
+			}
+		}
+		if($limit==0 && $offset==0) {
+			$query = $this->logdb->get($this->accountTable);
+		} else {
+			$query = $this->logdb->get($this->accountTable, $limit, $offset);
+		}
+		if($query->num_rows() > 0) {
+			return $query->result();
+		} else {
+			return false;
+		}
+	}
+	
+	public function update($id, $row)
+	{
+		if(!empty($id) && !empty($row))
+		{
+			$this->logdb->where('log_id', $id);
+			return $this->logdb->update($this->accountTable, $row);
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public function delete($id)
+	{
+		if(!empty($id))
+		{
+			$this->logdb->where('log_id', $id);
+			return $this->logdb->delete($this->accountTable);
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public function query($sql)
+	{
+		if(!empty($sql))
+		{
+			$query = $this->logdb->query($sql);
+			if($query->num_rows() > 0)
+			{
+				return $query->result();
+			}
+		}
+		return false;
+	}
+}
+
+?>
