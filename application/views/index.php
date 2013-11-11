ccae9b8de20cdc22c88ea902f3de8369e426db34
johnnyeven_work <johnnyeven@gmail.com>
2013-11-11 10:18:00 +0800

    1

--- application/views/index.php
+++ application/views/index.php
@@ -32,18 +32,8 @@
         </div>
         <div class="widget-content" >
           <div class="row-fluid">
-            <div class="span9">
+            <div class="span12">
               <div id="chartRegCount" class="chart"></div>
-            </div>
-            <div class="span3">
-              <ul class="site-stats">
-                <li class="bg_lh"><i class="icon-user"></i> <strong>2540</strong> <small>Total Users</small></li>
-                <li class="bg_lh"><i class="icon-plus"></i> <strong>120</strong> <small>New Users </small></li>
-                <li class="bg_lh"><i class="icon-shopping-cart"></i> <strong>656</strong> <small>Total Shop</small></li>
-                <li class="bg_lh"><i class="icon-tag"></i> <strong>9540</strong> <small>Total Orders</small></li>
-                <li class="bg_lh"><i class="icon-repeat"></i> <strong>10</strong> <small>Pending Orders</small></li>
-                <li class="bg_lh"><i class="icon-globe"></i> <strong>8540</strong> <small>Online Orders</small></li>
-              </ul>
             </div>
           </div>
         </div>
@@ -58,18 +48,8 @@
         </div>
         <div class="widget-content" >
           <div class="row-fluid">
-            <div class="span9">
+            <div class="span12">
               <div id="chartModifyCount" class="chart"></div>
-            </div>
-            <div class="span3">
-              <ul class="site-stats">
-                <li class="bg_lh"><i class="icon-user"></i> <strong>2540</strong> <small>Total Users</small></li>
-                <li class="bg_lh"><i class="icon-plus"></i> <strong>120</strong> <small>New Users </small></li>
-                <li class="bg_lh"><i class="icon-shopping-cart"></i> <strong>656</strong> <small>Total Shop</small></li>
-                <li class="bg_lh"><i class="icon-tag"></i> <strong>9540</strong> <small>Total Orders</small></li>
-                <li class="bg_lh"><i class="icon-repeat"></i> <strong>10</strong> <small>Pending Orders</small></li>
-                <li class="bg_lh"><i class="icon-globe"></i> <strong>8540</strong> <small>Online Orders</small></li>
-              </ul>
             </div>
           </div>
         </div>
@@ -80,7 +60,7 @@
     <div class="row-fluid">
       <div class="widget-box">
         <div class="widget-title bg_lg"><span class="icon"><i class="icon-signal"></i></span>
-          <h5>7天内登录人数变化趋势图</h5>
+          <h5>7天内活跃人数变化趋势图</h5>
         </div>
         <div class="widget-content" >
           <div class="row-fluid">
