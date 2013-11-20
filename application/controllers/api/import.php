c2204e3b4b22c06abd8e0084c4af2fa839f6b160
johnnyeven_work <johnnyeven@gmail.com>
2013-11-20 13:23:42 +0800

    1

--- application/controllers/api/import.php
+++ application/controllers/api/import.php
@@ -66,6 +66,43 @@ class Import extends CI_Controller
 			}
 		}
 	}
+	
+	public function mission_config_submit()
+	{
+		$this->load->library('Excel_MissionConfig_Adapter');
+		
+		$uploadDir = $this->config->item('upload_dir');
+		$error = "";
+		$msg = "";
+		$fileElementName = 'missionConfig';
+		$el = $this->input->get('el', TRUE);
+		if($el) {
+			$fileElementName = $el;
+		}
+		
+		$config['upload_path'] = $uploadDir;
+		$config['allowed_types'] = 'xls|xlsx';
+		$config['encrypt_name'] = TRUE;
+		
+		$this->load->library('upload', $config);
+		if (!$this->upload->do_upload($fileElementName)) {
+			$error = $this->upload->display_errors('<stronng>', '</stronng>');
+		} else {
+			$data = $this->upload->data();
+			$fileName = $uploadDir . '/' . $data['file_name'];
+		}
+		
+		if(!empty($fileName))
+		{
+			$result = $this->excel_missionconfig_adapter->ParseExcel($fileName);
+			$result = $this->excel_missionconfig_adapter->RemoveNull($result);
+			
+			if(!empty($result))
+			{
+				
+			}
+		}
+	}
 }
 
 ?>
