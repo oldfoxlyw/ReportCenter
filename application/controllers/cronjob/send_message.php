f00ad2ae31881f38cb724f355929e0f4c3d66c7f
johnnyeven_work <johnnyeven@gmail.com>
2013-11-25 10:01:20 +0800

    1

--- application/controllers/cronjob/send_message.php
+++ application/controllers/cronjob/send_message.php
@@ -13,7 +13,7 @@ class Send_message extends CI_Controller
 		$this->load->model('utils/connector');
 		
 		$ip = 'http://115.29.195.156:8090';
-		$content = '游戏内公告： 《冰与火之王》封测版本已于11月18日进行第二次大更新，请各位玩家及时从官网下载最新客户端，否则主线任务“紧急的战斗测试”等任务将永远无法完成。';
+		$content = '游戏内公告： 《冰火王座》封测版本已于11月18日进行第二次大更新，请各位玩家及时从官网下载最新客户端，否则主线任务“紧急的战斗测试”等任务将永远无法完成。';
 		
 		if(!empty($ip) && !empty($content))
 		{
