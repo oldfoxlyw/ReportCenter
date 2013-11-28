e54fa6675223f4bdd86eb365f9de06c596042120
johnnyeven_work <johnnyeven@gmail.com>
2013-11-28 14:20:59 +0800

    1

---
+++ application/controllers/order/equipment_sales.php
@@ -0,0 +1,70 @@
+<?php
+
+class Equipment_sales extends CI_Controller
+{
+	private $pageName = 'order/equipment_sales';
+	private $user = null;
+	
+	public function __construct()
+	{
+		parent::__construct ();
+		$this->load->model ( 'utils/check_user', 'check' );
+		$this->user = $this->check->validate ();
+		$this->check->permission ( $this->pageName );
+	}
+	
+	public function index()
+	{
+		$this->load->model ( 'mserver' );
+		
+		$serverResult = $this->mserver->read ();
+		$data = array (
+			'admin' => $this->user,
+			'page_name' => $this->pageName,
+			'server_result' => $serverResult,
+			'current_time' => time()
+		);
+		$this->render->render ( $this->pageName, $data );
+	}
+
+	public function lists($provider = 'highchart')
+	{
+		$this->load->model('mconsume');
+		
+		$serverId = $this->input->post('serverId');
+		$startTime = $this->input->post('startTime');
+		$endTime = $this->input->post('endTime');
+		
+		if(!empty($serverId) && !empty($startTime) && !empty($endTime))
+		{
+			$startTime = strtotime("{$startTime} 00:00:00");
+			$endTime = strtotime("{$endTime} 23:59:59");
+			$sql = "SELECT `item_level`, count(*) as `count`  FROM `log_consume` WHERE `server_id`='{$serverId}' AND `partner_key`='{$this->user->user_fromwhere}' AND `action_name`='buy_equipment' AND `log_time`>={$startTime} AND `log_time`<={$endTime} GROUP BY `item_level`";
+			$result = $this->mconsume->query($sql);
+
+			$data = array();
+			$data['axis'] = array();
+			$data['data'] = array();
+			
+			for($i=1; $i<=45; $i++)
+			{
+				array_push($data['axis'], $i);
+				$data['data'][$i] = 0;
+			}
+			
+			if(!empty($result))
+			{
+				foreach($result as $row)
+				{
+					$data['data'][intval($row->item_level)] = intval($row->count);
+				}
+			}
+			$data['data'] = array_values($data['data']);
+			
+			$this->load->model('utils/return_format');
+			echo $this->return_format->format($data);
+		}
+	}
+}
+
+?>
---
+++ application/views/order/equipment_sales.php
@@ -0,0 +1,204 @@
+<div id="content">
+<!--breadcrumbs-->
+<div id="content-header">
+    <div id="breadcrumb"> <span id="btnSwitchSidebar" class="badge margin-left-5 pointer" title="Close Sidebar"><i class="icon-chevron-left"></i><span> 关闭侧边栏</span></span><a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> 首页</a></div>
+</div>
+<!--End-breadcrumbs-->
+
+<!--Action boxes-->
+<div class="container-fluid">
+<!--End-Action boxes-->    
+    <div class="row-fluid">
+        <div class="widget-box">
+          <div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
+            <h5>搜索</h5>
+          </div>
+          <div class="widget-content nopadding">
+              <form action="" method="post" class="form-horizontal">
+                <div class="control-group">
+                    <label class="control-label">选择服务器</label>
+                    <div class="controls">
+                        <select id="serverId" name="serverId">
+                        <?php foreach($server_result as $server): ?>
+                            <option value="<?php echo $server->account_server_id; ?>"><?php echo $server->server_name; ?></option>
+                        <?php endforeach; ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="span6">
+                        <label class="control-label">开始时间(yyyy-mm-dd)</label>
+                        <div class="controls">
+                            <div data-date="<?php echo date('Y-m-d', $current_time - 7 * 86400); ?>" class="input-append date datepicker">
+                                <input type="text" id="startTime" name="startTime" value="<?php echo date('Y-m-d', $current_time - 7 * 86400); ?>"  data-date-format="yyyy-mm-dd" class="span11" >
+                                <span class="add-on"><i class="icon-th"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="span6">
+                        <label class="control-label">结束时间(yyyy-mm-dd)</label>
+                        <div class="controls">
+                            <div data-date="<?php echo date('Y-m-d', $current_time - 86400); ?>" class="input-append date datepicker">
+                                <input type="text" id="endTime" name="endTime" value="<?php echo date('Y-m-d', $current_time - 86400); ?>"  data-date-format="yyyy-mm-dd" class="span11" >
+                                <span class="add-on"><i class="icon-th"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-actions">
+                  <button id="btnSearch" type="button" class="btn btn-success">提交</button>
+                </div>
+              </form>
+          </div>
+        </div>
+    </div>
+<!--Chart-box-->    
+    <div class="row-fluid">
+      <div class="widget-box">
+          <div class="widget-title">
+            <ul class="nav nav-tabs">
+              <li class="active"><a data-toggle="tab" href="#tab1">图表</a></li>
+              <li><a data-toggle="tab" href="#tab2">数据</a></li>
+            </ul>
+          </div>
+          <div class="widget-content nopadding tab-content">
+            <div id="tab1" class="tab-pane active">
+                <div class="widget-content">
+                    <div class="row-fluid">
+                        <div id="chartRegCount"></div>
+                    </div>
+                </div>
+            </div>
+            <div id="tab2" class="tab-pane">
+                <div class="widget-content">
+                	<table class="table table-bordered data-table" id="listTable"></table>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+<!--End-Chart-box-->
+</div>
+</div>
+<link rel="stylesheet" href="<?php echo base_url('resources/css/datepicker.css'); ?>" />
+<link rel="stylesheet" href="<?php echo base_url('resources/css/select2.css'); ?>" />
+<script src="<?php echo base_url('resources/js/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/matrix.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/masked.js'); ?>"></script> 
+<script src="<?php echo base_url('resources/js/highcharts.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/bootstrap-datepicker.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/select2.min.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/jquery.dataTables.min.js'); ?>"></script>
+
+<script type="text/javascript">
+var dataTableHandler;
+$(function() {
+    $('.datepicker').datepicker();
+	
+	$("#btnSearch").click(function() {
+		if(dataTableHandler) dataTableHandler.fnDestroy();
+		$('#listTable').empty();
+		$.post("<?php echo site_url('order/equipment_sales/lists/highchart'); ?>", {
+			"serverId": $("#serverId").val(),
+			"startTime": $("#startTime").val(),
+			"endTime": $("#endTime").val(),
+			"equipmentName": $("#equipmentName").val()
+		}, onData);
+	});
+	$("select").select2();
+});
+
+function onData(data) {
+	if(!data)
+	{
+		return;
+	}
+	var json = eval("(" + data + ")");
+	
+	var column = [];
+	var aaData = [];
+	var series = [];
+	
+	column = [
+	{
+		"sTitle": "日期"
+	},
+	{
+		"sTitle": "销售数量"
+	}];
+	
+	var obj = {};
+	obj.name = "销售数量";
+	var data = [];
+	for(var i in json.data)
+	{
+		var rowData = [];
+		data.push(parseInt(json.data[i]));
+		rowData.push(json.axis[i]);
+		var spend = parseInt(json.data[i]);
+		rowData.push(spend);
+		aaData.push(rowData);
+	}
+	obj.data = data;
+	series.push(obj);
+	
+	$('#chartRegCount').highcharts({
+		chart: {
+			type: 'column',
+			height: 500
+		},
+		credits: {
+			enabled: false
+		},
+		title: {
+			text: '装备销售统计图'
+		},
+		subtitle: {
+			text: '数据来源：数据统计平台'
+		},
+		xAxis: {
+			categories: json.axis
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				text: '销售数量'
+			}
+		},
+		tooltip: {
+			crosshairs: [false, true]
+		},
+		series: series
+	});
+	dataTableHandler = $('#listTable').dataTable({
+		"bAutoWidth": false,
+		"bJQueryUI": true,
+		"bStateSave": true,
+		"iDisplayLength": 20,
+		"sPaginationType": "full_numbers",
+		"sDom": '<"H"lr>t<"F"fp>',
+        "aaData": aaData,
+        "aoColumns": column,
+		"oLanguage": {  
+			"sProcessing":   "处理中...",
+			"sLengthMenu":   "显示 _MENU_ 项结果",
+			"sZeroRecords":  "没有匹配结果",
+			"sInfo":         "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+			"sInfoEmpty":    "显示第 0 至 0 项结果，共 0 项",
+			"sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+			"sInfoPostFix":  "",
+			"sSearch":       "搜索:",
+			"sUrl":          "",
+			"oPaginate": {
+				"sFirst":    "首页",
+				"sPrevious": "上页",
+				"sNext":     "下页",
+				"sLast":     "末页"
+			}
+		}
+	});
+	
+	$("select").select2();
+}
+</script>
