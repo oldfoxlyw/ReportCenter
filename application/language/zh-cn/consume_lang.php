1fbb7c8e261e895fd99921d79ad093dc19324550
johnnyeven_work <johnnyeven@gmail.com>
2013-12-04 09:39:37 +0800

    1

--- application/language/zh-cn/consume_lang.php
+++ application/language/zh-cn/consume_lang.php
@@ -2,8 +2,8 @@
 $lang['consume_buy_equipment'] = '购买装备';
 $lang['consume_buy_item'] = '购买道具';
 $lang['consume_buy_mercenary_army'] = '购买雇佣军';
-$lang['consume_buy_retinue'] = '抽取随从(10绿钻)';
-$lang['consume_recruit_retinue'] = '十连抽随从';
+$lang['consume_buy_retinue'] = '十连抽随从';
+$lang['consume_recruit_retinue'] = '绿钻抽取随从';
 $lang['consume_buy_skill_points'] = '购买技能点';
 $lang['consume_encourage'] = '鼓舞';
 $lang['consume_get_dungeon_card_reward'] = '获取副本卡牌奖励';
@@ -13,5 +13,3 @@ $lang['consume_refresh_store'] = '刷新商店';
 $lang['consume_unlock_pos'] = '解锁阵形位置';
 $lang['consume_upgrade_equipment'] = '强化装备';
 $lang['consume_cpp_get_role_brief_info'] = '小游戏花费';
-
-// SELECT * FROM `log_account` WHERE `log_action`='ACCOUNT_LOGIN_SUCCESS' AND `log_time`>=1384012800 AND `log_time`<=1384099199 AND `log_GUID` in (SELECT `log_GUID` FROM `log_account` WHERE `log_action`='ACCOUNT_REGISTER_SUCCESS' AND `log_time`>=1383926400 AND `log_time`<=1384012799) GROUP BY `log_GUID`
