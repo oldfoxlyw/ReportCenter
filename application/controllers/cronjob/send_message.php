84e0e0c452425282f7742453ab9c92fd275eac4a
johnnyeven_home <johnnyeven@gmail.com>
2013-12-28 20:33:47 +0800

    1

--- application/controllers/cronjob/send_message.php
+++ application/controllers/cronjob/send_message.php
@@ -13,7 +13,7 @@ class Send_message extends CI_Controller
 		$this->load->model('utils/connector');
 		
 		$ip = 'http://115.29.195.156:8090';
-		$content = '临近公测，《冰火王座》封测版游戏攻略征集活动中奖名单也火热出炉啦，快加入冰火官方群（38820749），联系群主“冰火”，看看有没有你的名字吧！';
+		$content = '冰火王座盛大开测，7*24小时劲爆活动惊喜乐不停！每晚11点55，免费抢宝石！每天上午12点和晚上8点，全区狂欢，抢夺极品红包！关注冰火王座官方网站(bhwz.zqgame.com)，加入冰火官方群（38820749），尽享更多精彩活动内容！';
 		
 		if(!empty($ip) && !empty($content))
 		{
