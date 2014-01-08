c9e7429bfdf314a68b64c5c6f63c32eb65af9cd2
johnnyeven_work <johnnyeven@gmail.com>
2014-01-08 13:40:06 +0800

    1

--- application/controllers/cronjob/send_message.php
+++ application/controllers/cronjob/send_message.php
@@ -13,7 +13,7 @@ class Send_message extends CI_Controller
 		$this->load->model('utils/connector');
 		
 		$ip = 'http://183.60.255.57:8091';
-		$content = '尊敬的各位玩家，《冰火王座》临近正式上线，原测试服红龙女王，将于1月4号晚24时关闭，越狱渠道封测将于10号开启，有许多新的活动和奖品，欢迎各位新老家去试玩！更多信息以及详情请关注官网！bhwz.zqgame.com';
+		$content = '《冰火王座》火热开服，7X24小时劲爆活动不断，丰厚奖励拿到你手软！心动不如行动，每天记得准时上线参加哦！';
 		
 		if(!empty($ip) && !empty($content))
 		{
