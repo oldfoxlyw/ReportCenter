183da561a994cadd680fed9ca4535ef9683ac11c
johnnyeven_work <johnnyeven@gmail.com>
2014-05-26 14:11:20 +0800

    1

--- application/views/index.php
+++ application/views/index.php
@@ -182,7 +182,7 @@ function retrieveTableData(serverId) {
 		"sServerMethod": "POST",
 		"aoColumns": [
 			{"mData": "log_date", "bSortable": false},
-			{"mData": "valid_account"},
+			{"mData": "reg_account"},
 			{"mData": "login_account"},
 			{"mData": "level_account"},
 			{"mData": "dau"},
