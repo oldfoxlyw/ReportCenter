1bdd5c44995e43a80c410a042cc2504856e63c5f
johnnyeven_work <johnnyeven@gmail.com>
2014-07-17 16:41:40 +0800

    1

--- application/controllers/channel/zhuanhua.php
+++ application/controllers/channel/zhuanhua.php
@@ -35,11 +35,33 @@ class Zhuanhua extends CI_Controller
 		$offset = $this->input->get_post('iDisplayStart');
 		$limit = $this->input->get_post('iDisplayLength');
 		$keyword = $this->input->get_post('sSearch');
+		$starttime = $this->input->get_post('starttime');
+		$endtime = $this->input->get_post('endtime');
 
 		$limit = $limit < 0 ? 25 : $limit;
+		if(!empty($starttime))
+		{
+			$channeldb->where('time >=', "{$starttime} 00:00:00");
+		}
+		if(!empty($endtime))
+		{
+			$channeldb->where('time <=', "{$endtime} 23:59:59");
+		}
 		$count = $channeldb->count_all_results('click_table');
 
-		$result = $channeldb->query("select * from click_table order by time desc limit {$limit} offset {$offset}");
+		if(!empty($starttime))
+		{
+			$channeldb->where('time >=', "{$starttime} 00:00:00");
+		}
+		if(!empty($endtime))
+		{
+			$channeldb->where('time <=', "{$endtime} 23:59:59");
+		}
+		$channeldb->order_by('time', 'desc');
+		$channeldb->limit($limit);
+		$channeldb->offset($offset);
+		$result = $channeldb->get('click_table');
+		// $result = $channeldb->query("select * from click_table order by time desc limit {$limit} offset {$offset}");
 		$result = $result->result();
 
 		$data = array(
@@ -61,12 +83,32 @@ class Zhuanhua extends CI_Controller
 		$offset = $this->input->get_post('iDisplayStart');
 		$limit = $this->input->get_post('iDisplayLength');
 		$keyword = $this->input->get_post('sSearch');
+		$starttime = $this->input->get_post('starttime');
+		$endtime = $this->input->get_post('endtime');
 
 		$limit = $limit < 0 ? 25 : $limit;
-		$result = $channeldb->query("select count(*) as `count` from valid_click group by ip")->result();
-		$count = count($result);
+		if(!empty($starttime))
+		{
+			$channeldb->where('date >=', $starttime);
+		}
+		if(!empty($endtime))
+		{
+			$channeldb->where('date <=', $endtime);
+		}
+		$count = $channeldb->count_all_results('valid_click');
 
-		$result = $channeldb->query("select * from valid_click group by ip order by date desc limit {$limit} offset {$offset}");
+		if(!empty($starttime))
+		{
+			$channeldb->where('date >=', $starttime);
+		}
+		if(!empty($endtime))
+		{
+			$channeldb->where('date <=', $endtime);
+		}
+		$channeldb->order_by('date', 'desc');
+		$channeldb->limit($limit);
+		$channeldb->offset($offset);
+		$result = $channeldb->get('valid_click');
 		$result = $result->result();
 
 		$data = array(
