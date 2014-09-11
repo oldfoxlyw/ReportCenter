9df043f3929025ca4f7c471118abe205877d1418
johnnyeven_work <johnnyeven@gmail.com>
2014-09-11 10:28:39 +0800

    1

---
+++ application/controllers/order/consume_mvp.php
@@ -0,0 +1,68 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Consume_mvp extends CI_Controller
+{
+	private $pageName = 'order/consume_mvp';
+	private $user = null;
+
+	public function __construct()
+	{
+		parent::__construct ();
+		$this->load->model('utils/check_user', 'check');
+		$this->user = $this->check->validate();
+		$this->check->permission($this->pageName);
+	}
+	
+	public function index()
+	{
+		$this->load->model('mserver');
+		$this->load->model('mpartner');
+		
+		$serverResult = $this->mserver->read();
+		$partnerResult = $this->mpartner->read();
+		$data = array(
+			'admin'			=>	$this->user,
+			'page_name'		=>	$this->pageName,
+			'server_result'	=>	$serverResult,
+			'current_time'	=>	time(),
+			'partner_result'=>	$partnerResult
+		);
+		$this->render->render($this->pageName, $data);
+	}
+	
+	public function lists($provider = 'highchart')
+	{
+		$this->load->model('utils/return_format');
+		$accountdb = $this->load->database('fundsdb', TRUE);
+		
+		$serverId = $this->input->post('serverId');
+		$startTime = $this->input->post('startTime');
+		$endTime = $this->input->post('endTime');
+		$partnerKey = $this->input->post('partnerKey');
+		$rechargeSum = $this->input->post('rechargeSum');
+		
+		if(!empty($startTime) && !empty($endTime) && !empty($serverId))
+		{
+			$startTime = strtotime("{$startTime} 00:00:00");
+			$endTime = strtotime("{$endTime} 23:59:59");
+
+			if(!empty($partnerKey))
+			{
+				$partner = "AND `partner_key`='{$partnerKey}'";
+			}
+			else
+			{
+				$partner = '';
+			}
+
+			$rechargeSum = empty($rechargeSum) ? 0 : -intval($rechargeSum);
+			
+			$sql = "SELECT `account_guid`, `account_nickname`, `server_id`, SUM(`funds_item_amount`) AS `funds_item_amount` FROM `funds_checkinout` WHERE `funds_flow_dir`='CHECK_OUT' AND `server_id`='{$serverId}' AND `funds_time`>={$startTime} AND `funds_time`<={$endTime} {$partner} GROUP BY `account_guid` HAVING `funds_item_amount`<={$rechargeSum} ORDER BY `funds_item_amount` DESC";
+			$result = $accountdb->query($sql)->result();
+			
+			echo $this->return_format->format($result);
+		}
+	}
+}
+
+?>
---
+++ application/views/order/consume_mvp.php
@@ -0,0 +1,190 @@
+<div id="content">
+<!--breadcrumbs-->
+<div id="content-header">
+    <div id="breadcrumb"> <span id="btnSwitchSidebar" class="badge margin-left-5 pointer" title="Close Sidebar"><i class="icon-chevron-left"></i><span> 关闭侧边栏</span></span><a href="<?php echo site_url('index'); ?>" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> 首页</a></div>
+</div>
+<!--End-breadcrumbs-->
+
+<!--Action boxes-->
+<div class="container-fluid">
+<!--End-Action boxes-->    
+    <div class="row-fluid">
+        <div class="widget-box">
+          <div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
+            <h5>搜索</h5>
+          </div>
+          <div class="widget-content nopadding">
+              <form action="" method="post" class="form-horizontal">
+                <div class="control-group">
+                    <label class="control-label">选择服务器</label>
+                    <div class="controls">
+                        <select id="serverId" name="serverId">
+                        <?php foreach($server_result as $server): ?>
+                            <option value="<?php echo $server->account_server_id; ?>"><?php echo $server->server_name; ?></option>
+                        <?php endforeach; ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="span6">
+                        <label class="control-label">开始时间(yyyy-mm-dd)</label>
+                        <div class="controls">
+                            <div data-date="<?php echo date('Y-m-d', $current_time - 6 * 86400); ?>" class="input-append date datepicker">
+                                <input type="text" id="startTime" name="startTime" value="<?php echo date('Y-m-d', $current_time - 6 * 86400); ?>"  data-date-format="yyyy-mm-dd" >
+                                <span class="add-on"><i class="icon-th"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="span6">
+                        <label class="control-label">结束时间(yyyy-mm-dd)</label>
+                        <div class="controls">
+                            <div data-date="<?php echo date('Y-m-d', $current_time); ?>" class="input-append date datepicker">
+                                <input type="text" id="endTime" name="endTime" value="<?php echo date('Y-m-d', $current_time); ?>"  data-date-format="yyyy-mm-dd" >
+                                <span class="add-on"><i class="icon-th"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label">选择渠道</label>
+                    <div class="controls">
+                        <select id="partnerKey" name="partnerKey">
+                            <option value="">全部</option>
+                        <?php foreach($partner_result as $partner): ?>
+                            <option value="<?php echo $partner->partner_key; ?>"><?php echo $partner->partner_key; ?></option>
+                        <?php endforeach; ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label">绿钻消耗总额阀值</label>
+                    <div class="controls">
+                    	<input name="rechargeSum" id="rechargeSum" type="text" placeholder="数字" value="" />
+                        <span class="help-block">绿钻消耗总额</span>
+                    </div>
+                </div>
+                <div class="form-actions">
+                  <button id="btnSearch" type="button" class="btn btn-success">提交</button>
+                </div>
+              </form>
+          </div>
+        </div>
+    </div>
+<!--Chart-box-->    
+    <div class="row-fluid">
+      <div class="widget-box">
+          <div class="widget-title">
+            <ul class="nav nav-tabs">
+              <li class="active"><a data-toggle="tab" href="#tab1">数据</a></li>
+              <li><a data-toggle="tab" href="#tab2">生成发送礼包的昵称列表</a></li>
+            </ul>
+          </div>
+          <div class="widget-content nopadding tab-content">
+            <div id="tab1" class="tab-pane active">
+                <table class="table table-bordered data-table" id="listTable"></table>
+            </div>
+            <div id="tab2" class="tab-pane">
+                <div class="widget-content">
+                    <div class="row-fluid">
+                        <div id="chartRegCount"></div>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+<!--End-Chart-box-->
+</div>
+</div>
+<link rel="stylesheet" href="<?php echo base_url('resources/css/datepicker.css'); ?>" />
+<link rel="stylesheet" href="<?php echo base_url('resources/css/select2.css'); ?>" />
+<script src="<?php echo base_url('resources/js/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/matrix.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/masked.js'); ?>"></script> 
+<script src="<?php echo base_url('resources/js/highcharts.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/bootstrap-datepicker.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/select2.min.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/jquery.dataTables.min.js'); ?>"></script>
+
+<script type="text/javascript">
+var dataTableHandler;
+
+$(function() {
+    $('.datepicker').datepicker();
+	$("#rechargeSum").mask("?9999999999");
+	$("#btnSearch").click(function() {
+		if(dataTableHandler) dataTableHandler.fnDestroy();
+		$('#listTable').empty();
+		$.post("<?php echo site_url('order/recharge_mvp/lists/highchart'); ?>", {
+			"serverId": $("#serverId").val(),
+			"startTime": $("#startTime").val(),
+			"endTime": $("#endTime").val(),
+			"partnerKey": $("#partnerKey").val(),
+			"rechargeSum": $("#rechargeSum").val()
+		}, onData);
+	});
+	$("select").select2();
+});
+
+function onData(data) {
+	if(!data)
+	{
+		return;
+	}
+	var json = eval("(" + data + ")");
+
+	var column = [
+	{
+		"sTitle": "GUID"
+	},
+	{
+		"sTitle": "昵称"
+	},
+	{
+		"sTitle": "服务器编号"
+	},
+	{
+		"sTitle": "绿钻消耗总额"
+	}];
+	var aaData = [];
+	var nickname = "";
+	for(var i in json) {
+		var row = [json[i].account_guid, json[i].account_nickname, json[i].server_id, json[i].funds_amount / 100];
+		aaData.push(row);
+
+		nickname += json[i].account_nickname + ",";
+	}
+	nickname = nickname.substring(0, nickname.length - 1);
+	$("#chartRegCount").text(nickname);
+
+	dataTableHandler = $('#listTable').dataTable({
+		"bAutoWidth": false,
+		"bJQueryUI": true,
+		"bStateSave": true,
+		"iDisplayLength": 20,
+		"sPaginationType": "full_numbers",
+		"sDom": '<"H"lr>t<"F"fp>',
+        "aaData": aaData,
+        "aoColumns": column,
+		"oLanguage": {  
+			"sProcessing":   "处理中...",
+			"sLengthMenu":   "显示 _MENU_ 项结果",
+			"sZeroRecords":  "没有匹配结果",
+			"sInfo":         "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+			"sInfoEmpty":    "显示第 0 至 0 项结果，共 0 项",
+			"sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+			"sInfoPostFix":  "",
+			"sSearch":       "搜索:",
+			"sUrl":          "",
+			"oPaginate": {
+				"sFirst":    "首页",
+				"sPrevious": "上页",
+				"sNext":     "下页",
+				"sLast":     "末页"
+			}
+		}
+	});
+	$("select").select2();
+}
+</script>
