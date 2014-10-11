837b9aa4761563e9fe60b3ef2fa2ac81d2ece2ce
johnnyeven_work <johnnyeven@gmail.com>
2014-10-11 14:08:24 +0800

    1

--- application/views/order/consume_detail.php
+++ application/views/order/consume_detail.php
@@ -165,7 +165,7 @@ function onData(data) {
         "aaData": aaData,
         "aoColumns": column,
         "aaSorting": [
-        	[4, 'desc']
+        	[5, 'desc']
         ],
 		"oLanguage": {  
 			"sProcessing":   "处理中...",
