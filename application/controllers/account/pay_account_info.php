f86dc2c55b2298d162e00506379ad49fc319986c
johnnyeven_work <johnnyeven@gmail.com>
2014-11-25 10:09:06 +0800

    1

--- application/controllers/account/pay_account_info.php
+++ application/controllers/account/pay_account_info.php
@@ -64,6 +64,8 @@ class Pay_account_info extends CI_Controller
 				$row = array_merge((array)$result[0], (array)$info[0]);
 				array_push($data, $row);
 			}
+		echo $this->return_format->format($data);
+		exit();
 
 			for($i = 0; $i < count($data); $i++)
 			{
